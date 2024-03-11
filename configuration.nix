871752761ce8ae3caec7947b7446dc90db89eb1b
Gabriel Schneider <gabrielschneider100@protonmail.com>
2024-03-10 21:40:21 -0300

    gen-79:2024-03-10

--- configuration.nix
+++ configuration.nix
@@ -44,6 +44,7 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
   services.xserver.dpi = 240;
+  services.xserver.wacom.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
