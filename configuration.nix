67c500ad5e62d21079ac50f6c977a12b823690e1
Gabriel Schneider <gabrielschneider100@protonmail.com>
2024-03-15 15:45:02 -0300

    gen-99:2024-03-15

--- configuration.nix
+++ configuration.nix
@@ -263,7 +263,33 @@
   services.tailscale.enable = true;
    services.openssh = {
      enable = true;
-   };
+  };
+  programs.sway = {
+    enable = true;
+    wrapperFeatures.gtk = true; # so that gtk works properly
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      wl-clipboard
+      wf-recorder
+      mako # notification daemon
+      grim
+       #kanshi
+      slurp
+      alacritty # Alacritty is the default terminal in the config
+      #dmenu # Dmenu is the default in the config but i recommend wofi since its wayland native
+      wofi
+    ];
+    extraSessionCommands = ''
+      export SDL_VIDEODRIVER=wayland
+      export QT_QPA_PLATFORM=wayland
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+      export _JAVA_AWT_WM_NONREPARENTING=1
+      export MOZ_ENABLE_WAYLAND=1
+    '';
+  };
+  programs.waybar.enable = true;
+
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
