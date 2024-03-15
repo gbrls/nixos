9f559395bea7df045590f1dfcb2d7d902a0328eb
Gabriel Schneider <gabrielschneider100@protonmail.com>
2024-03-15 18:33:36 -0300

    gen-100:2024-03-15

--- vim.nix
+++ vim.nix
@@ -56,10 +56,10 @@
     ];
     colorschemes.melange = {
       enable = true;
-      italic = false;
     };
     globals.mapleader = " ";
     options = {
+      italic = false;
       number = true; relativenumber = true; scrolloff = 3;
       tabstop = 4; softtabstop = 4; shiftwidth = 4; expandtab = true;
       breakindent = true;
